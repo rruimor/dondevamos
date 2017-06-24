4ab5dcb9771afb51b303be51db7b5b370bf11703
Rafael Ruiz <rafael@independentip.com>
2017-06-24 18:09:40 +0200

    Initial commit

---
+++ lib/dondevamos.ex
@@ -0,0 +1,18 @@
+defmodule Dondevamos do
+  @moduledoc """
+  Documentation for Dondevamos.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dondevamos.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dondevamos_test.exs
@@ -0,0 +1,8 @@
+defmodule DondevamosTest do
+  use ExUnit.Case
+  doctest Dondevamos
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
